17ca02a176ae640867d76fa0437d187db63523b5
Zain4391 <manutdfast91@gmail.com>
2025-08-01 20:37:56 +0500

    CORS fix

--- payment-service/app.js
+++ payment-service/app.js
@@ -13,7 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(helmet());
 app.use(errorHandler());
 
